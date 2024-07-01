401b5c03defdb8f069880049bdd8013ac6dacc5b
GabrielPepe <gabrielpepe.pp@gmail.com>
2024-07-01 19:16:53 -0300

    succeeded_trans

--- selector-server/src/checkHour.ts
+++ selector-server/src/checkHour.ts
@@ -1,4 +1,18 @@
+import { exec } from "child_process";
+
 export async function checkHour() {
-    const selectorHour = Date()
-    const hour = await fetch("http://bank:3000/banco/hora").then(res => res.json()).then(res => res.date)
+  const selectorTime = Date.now();
+
+  const bankTimeResponse = await fetch("http://bank:3000/banco/hora");
+  const bankTimeData = await bankTimeResponse.json();
+  const bankTime = new Date(bankTimeData.date).getTime();
+
+  const offset = bankTime - selectorTime;
+
+  const adjustedSelectorTime = new Date(selectorTime + offset);
+
+  console.log(`Horário ajustado do seletor: ${adjustedSelectorTime}`);
+
+  const command = `date -s "${adjustedSelectorTime.toISOString()}"`;
+  exec(command);
 }
--- selector-server/src/controllers/transaction/finishTransaction.ts
+++ selector-server/src/controllers/transaction/finishTransaction.ts
@@ -1,72 +1,110 @@
-import { prisma } from "../../../prisma/prisma"
+import { prisma } from "../../../prisma/prisma";
 
-export async function finishTransaction(transaction:any, validations: { APPROVED: string[], DENIED: string[] }) {
-    
-    const mostValidation = validations.APPROVED.length > validations.DENIED.length ? "APPROVED" : "DENIED"
-    const minValidation = mostValidation == "APPROVED" ? "DENIED" : "APPROVED"
-    
-    const qtyValidators = validations[mostValidation].length
-    const validatorTax = (transaction.trans_tax * 0.66)/qtyValidators
-    
-    // UPDATING TRANSACTION STATE
-    await prisma.transaction.update({
-        where: {
-            trans_id: transaction.trans_id
-        },
-        data: {
-            trans_state: mostValidation,
-        }
-    })
-    
-    // ADJUSTING FLAGS
+export async function finishTransaction(
+  transaction: any,
+  validations: { APPROVED: string[]; DENIED: string[] },
+  validators: []
+) {
+  const mostValidation =
+    validations.APPROVED.length > validations.DENIED.length
+      ? "APPROVED"
+      : "DENIED";
+  const minValidation = mostValidation == "APPROVED" ? "DENIED" : "APPROVED";
+
+  const qtyValidators = validations[mostValidation].length;
+  const validatorTax = (transaction.trans_tax * 0.66) / qtyValidators;
+
+  // UPDATING TRANSACTION STATE
+  await prisma.transaction.update({
+    where: {
+      trans_id: transaction.trans_id,
+    },
+    data: {
+      trans_state: mostValidation,
+    },
+  });
+
+  // ADJUSTING FLAGS
+  await prisma.validator.updateMany({
+    where: {
+      validator_id: {
+        in: validations[minValidation],
+      },
+    },
+    data: {
+      flag: {
+        increment: 1,
+      },
+    },
+  });
+
+  if (mostValidation == "APPROVED") {
+    // INCREMENT SUCCEEDED TRANS
     await prisma.validator.updateMany({
-        where: {
-            validator_id: {
-                in: validations[minValidation]
-            }
+      where: {
+        validator_id: {
+          in: validations[mostValidation],
         },
-        data: {
-            flag: {
-                increment: 1
-            }
-        }
-    })
-    
-    if (mostValidation == 'APPROVED') {
-        await prisma.address.update({
-            where: {
-                address: transaction.receiver_address
-            },
-            data: {
-                coins_in_stock: {
-                    increment: transaction.trans_coins
-                }
-            }
-        })
-        
-        await prisma.address.update({
-            where: {
-                address: transaction.sender_address
-            },
-            data: {
-                coins_in_stock: {
-                    decrement: transaction.trans_coins + transaction.trans_tax
-                }
-            }
-        })
-        
-        await prisma.validatorTransaction.updateMany({
-            where: {
-                trans_id: transaction.trans_id,
-                AND: {
-                    validator_id: {
-                        in: validations.APPROVED
-                    }
-                }
-            },
-            data: {
-                validator_tax: validatorTax,   
-            }
-        })
-    }
+      },
+      data: {
+        succeeded_trans: {
+          increment: 1,
+        },
+      },
+    });
+
+    await prisma.validator.updateMany({
+      where: {
+        flag: {
+          gt: 0,
+        },
+        AND: {
+          succeeded_trans: {
+            gt: 9999,
+          },
+        },
+      },
+      data: {
+        flag: {
+          decrement: 1,
+        },
+      },
+    });
+
+    await prisma.address.update({
+      where: {
+        address: transaction.receiver_address,
+      },
+      data: {
+        coins_in_stock: {
+          increment: transaction.trans_coins,
+        },
+      },
+    });
+
+    await prisma.address.update({
+      where: {
+        address: transaction.sender_address,
+      },
+      data: {
+        coins_in_stock: {
+          decrement: transaction.trans_coins + transaction.trans_tax,
+        },
+      },
+    });
+
+    await prisma.validatorTransaction.updateMany({
+      where: {
+        trans_id: transaction.trans_id,
+        AND: {
+          validator_id: {
+            in: validations.APPROVED,
+          },
+        },
+      },
+      data: {
+        validator_tax: validatorTax, // shouldn't it be increment??
+      },
+    });
+  }
 }
